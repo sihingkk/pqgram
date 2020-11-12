ad73415548dadd1be27b392b94a1eddd7e6b8bfb
Chris Kaczmarek <chris.kaczmarek@neotechnology.com>
2020-11-12 22:53:44 +0000

    removed unused code

--- src/plans.clj
+++ src/plans.clj
@@ -4,27 +4,6 @@
             [pqgram.db :refer [local-db]]
             [clojure.zip :as z]))
 
-(db/defquery root-operator
-  "MATCH (testRun:TestRun {id:$id})-[:HAS_METRICS]->(metrics:Metrics)-[:HAS_PLAN]->(plan:Plan),
-   (plan)-[:HAS_PLAN_TREE]->(tree:PlanTree)-[:HAS_OPERATORS]->(operator:Operator),
-   p = (operator:Operator)
-   RETURN operator.operator_type as name, id (operator) as id")
-
-(defn root [id]
-  (with-open [session (db/get-session local-db)]
-    (first (root-operator session {:id id}))))
-
-
-(db/defquery operator-children
-  "MATCH (operator:Operator)-[:HAS_CHILD]->(children:Operator)
-   WHERE id (operator) = $id   
-   RETURN children.operator_type as name, id(children) as id")
-
-(defn children-by-id [id]
-  (with-open [session (db/get-session local-db)]
-    (vec (doall  (operator-children session {:id id})))))
-
-
 (db/defquery test-run-ids
   "MATCH (m:Metrics)-[:METRICS_FOR]->(b:Benchmark )<-[:HAS_BENCHMARK]-(bg:BenchmarkGroup {name: $name}),
          (m:Metrics)<-[:HAS_METRICS]-(tr:TestRun)-[:WITH_PROJECT]->(p:Project {commit: $commit})
@@ -34,17 +13,6 @@
   (with-open [session (db/get-session local-db)]
     (doall  (mapv :id (test-run-ids session {:commit commit :name group-name})))))
 
-(defn tree
-
-  ([operator]
-   (tree operator []))
-
-  ([{:keys [name id]} acc]
-   (let [children (children-by-id  id)]
-     (if (empty? children)
-       name
-       (vec (concat acc [name] (mapv tree children)))))))
-
 (defn tree-zipper [tree]
   (z/zipper vector?
             rest
@@ -53,11 +21,6 @@
             tree))
 
 (def d (pq-gram-distance tree-zipper 3 2))
-
-(defn ->tree [test-run-id]
-  (tree (root test-run-id)))
-
-
 
 (def example-id
   (first (find-test-run-ids {:commit "0b7d6e27a7cd0534b1a9f27607b1b99ab6444774"
@@ -180,13 +143,3 @@
   
   )
   
-
-
-
-
-
-
-
-
-
-
